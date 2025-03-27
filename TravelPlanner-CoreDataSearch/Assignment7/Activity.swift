78235f8cb80a92f65db2f49b1dc0152db6231f2c
Vikas Meneni <meneni.v@northeastern.edu>
2025-03-26 20:02:54 -0400

    assignment 8 commit

---
+++ TravelPlanner-CoreDataSearch/Assignment7/Activity.swift
@@ -0,0 +1,10 @@
+/*import Foundation
+
+struct Activity {
+    var id: Int
+    var tripId: Int  // Links to a trip
+    var name: String
+    var date: String
+    var time: String
+    var location: String
+}*/
