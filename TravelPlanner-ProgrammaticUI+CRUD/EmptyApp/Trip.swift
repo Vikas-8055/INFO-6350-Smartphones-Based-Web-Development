5b14cd197ae86a1638d02ccfe07645d296b458ef
Vikas-8055 <meneni.v@northeastern.edu>
2025-04-13 17:59:02 -0400

    assignment 6 commit

---
+++ TravelPlanner-ProgrammaticUI+CRUD/EmptyApp/Trip.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Trip {
+    var id: Int
+    var destination_id: Int
+    var title: String
+    var start_date: String
+    var end_date: String
+    var description: String
+}
